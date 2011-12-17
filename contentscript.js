6c27cb70c3081d296882571392195cf8c27579d7
fititnt <emerson@webdesign.eng.br>
2011-12-17 01:21:54 -0200

    Renamed from JChrome-for-Google-Chrome to JFox-for-Google-Chrome and make this repository public

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,39 @@
+var now = new Date();
+var hour        = now.getHours();
+var minute      = now.getMinutes();
+var second      = now.getSeconds();
+
+
+if($("jchrome")){
+    console.groupCollapsed('JChrome [' + hour + ':' + minute + ':' + second + ']: ' + window.location);
+    //console.log($("jchrome").innerHTML);//console.dir
+    if($("jchrome-page")){
+        console.info("Page",JSON.parse($("jchrome-page").innerHTML) );//console.dir
+    }
+    if($("jchrome-performance")){
+        console.info("performance",JSON.parse($("jchrome-performance").innerHTML) );//console.dir
+    }
+    if($("jchrome-system")){
+        console.info("System",JSON.parse($("jchrome-system").innerHTML) );//console.dir
+    }
+    if($("jchrome-security")){
+        console.info("Security",JSON.parse($("jchrome-security").innerHTML) );//console.dir
+    }
+    console.groupEnd('jchrome');
+} else {
+    console.log("JChrome is not installed or enabled for this page");//console.dir
+}
+
+
+
+
+
+
+
+
+
+
+
 //alert('content script');
 
 /*
@@ -8,6 +44,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
    sendResponse({}); // snub them.
 });
 */
+
 chrome.extension.sendRequest({greeting: "hello"}, function(response) {
   console.log(response.farewell);
 });
@@ -15,16 +52,19 @@ chrome.extension.sendRequest({greeting: "hello"}, function(response) {
 //chrome.extension.sendRequest({greeting: "hello"}, function(response) {
 //  console.log(response.farewell);
 //});
-/*
+
 //console.log(document.getElementById("system-debug"));
-console.log(document.getElementById("jfox_general"));//console.dir
-console.log(document.getElementById("jfox_system"));
-console.log(document.getElementById("jfox_page"));
-console.log(document.getElementById("jfox_queries"));
-console.log(document.getElementById("jfox_request"));
-console.log(document.getElementById("jfox_permissions"));
+
+
+
+//console.log(document.getElementById("jchrome_general"));//console.dir
+//console.log(document.getElementById("jchrome_system"));
+//console.log(document.getElementById("jchrome_page"));
+//console.log(document.getElementById("jchrome_queries"));
+//console.log(document.getElementById("jchrome_request"));
+//console.log(document.getElementById("jchrome_permissions"));
 //console.log(document.getElementById("jfox_general"));
-*/
+
 
 /**
  * Alias for document.getElementById.
